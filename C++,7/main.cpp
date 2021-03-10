b555a8c3634e8f6d0bac8907d9d77d7dd3747d61
Zeynep Serra Akkaya <78758154+zeynepserraakkaya@users.noreply.github.com>
2021-03-10 11:02:02 +0300

    C++,7-while, do

---
+++ C++,7/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i=1;
+
+    while(i<5){
+        cout<<i<<endl;
+        i++;
+    }
+
+    i=1;
+
+    while(i<=5){
+        cout<<i<<endl;
+        i++;
+    }
+
+    i=6;
+
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(i<=5);
+
+    return 0;
+}
